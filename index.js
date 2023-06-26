06714affb180850382a46eb4c7f044b30705dd21
John Alarcon <j.alarcon@outlook.com>
2023-06-26 16:59:39 -0400

    fixed minor typos

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function writeToFile(fileName, answers) {
     let shapeChoice;
     if (answers.shape === "Square") {
         shapeChoice = new Square();
-        svgString += `<rect x="75" y="40" width="160" height="160" fill="${answers.backgroundColor}"/>`;
+        svgString += `<rect x="75" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
     } else if (answers.shape === "Circle") {
         shapeChoice = new Circle();
-        svgString += `<circle cx="150" y="110" r="75" fill="${answers.backgroundColor}"/>`;
+        svgString += `<circle cx="150" y="110" r="75" fill="${answers.shapeColor}"/>`;
     } else {
         shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 20 250, 180 50, 180" fill="${answers.backgroundColor}"/>`;
+        svgString += `<polygon points="150, 20 250, 180 50, 180" fill="${answers.shapeColor}"/>`;
     }
 
     svgString += `<text x="150" y="120" font-size="40" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
@@ -40,7 +40,7 @@ function promptUser() {
             {
                 type: "input",
                 message: "Choose the desired color for the text (keyword OR hexadecimal number)",
-                name:"text",
+                name:"textColor",
             },
             {
                 type: "list",
@@ -51,7 +51,7 @@ function promptUser() {
             {
                 type: "input",
                 message: "Choose the desired color for the logo shape (keyword OR hexadecimal number)",
-                name:"backgroundColort",
+                name:"shapeColor",
             },
         ])
         .then((answers) => {
